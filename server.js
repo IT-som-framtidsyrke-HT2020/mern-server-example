d014709a6e8cd5f91e52d23f9afa39b166fefb8f
Helena Johansson <helena@klarr.se>
2021-01-22 11:35:12 +0100

    handle error

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const colors = require("colors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -38,12 +39,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
 process.on("unhandleRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server and exit
   server.close(() => process.exit());
 });
