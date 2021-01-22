db9e3aa4facc5cdad0f9b74d85a3326d69a4407b
Helena Johansson <helena@klarr.se>
2021-01-22 11:29:23 +0100

    server connection finished

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Routes
+
+// Connect databse
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Enable cors
+app.use(cors());
+
+// Mount routes
+app.get("/api/v1/", (req, res) => {
+  res.send("Hello from express");
+});
+
+// Routes above errorhandler
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on("unhandleRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit
+  server.close(() => process.exit());
+});
