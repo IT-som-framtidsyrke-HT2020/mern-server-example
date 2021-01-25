6a017fa07fd15b6b806f2bf6a7d5a9fd7822eb14
Helena Johansson <helena@klarr.se>
2021-01-25 11:28:56 +0100

    signin

--- controllers/user.js
+++ controllers/user.js
@@ -19,3 +19,28 @@ exports.signup = (req, res) => {
     });
   });
 };
+
+// login user
+exports.signin = (req, res) => {
+  //find user based on email
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User doesnt exist, please signup",
+      });
+    }
+    //if user is found make sure the email and pw match
+    //user authenticate method is created in model
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and password dont match",
+      });
+    }
+    //generate signed token with user id and secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    //return repsonse with user and token to frontend client
+    const { _id, name, email } = user;
+    return res.json({ token, user: { _id, email, name } });
+  });
+};
--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 const { v4: uuidv4 } = require("uuid");
 
-const UserSchema = mongoose.schema(
+const UserSchema = mongoose.Schema(
   {
     name: {
       type: String,
